c30a575df8b183d8dffe4734a47dfa869013071e
Andres Arias <aariasc@gmail.com>
2020-09-09 16:51:47 -0600

    Documentacion

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,37 @@
 export interface User {
+  /**
+   * firstName: nombre de la persona
+   */
   firstName: string;
+
+  /**
+   * lastName: apellido de la persona
+   */
   lastName: string;
+
+  /**
+   * age: edad de la persona. Numerico sin decimales
+   */
   age: number;
+
+  /**
+   * email: correo electronico
+   */
   email: string;
+
+  /**
+   * favoriteColor: nombre del color favorito
+   */
   favoriteColor: string;
+
+  /**
+   * foobar: campo magico (opcional)
+   */
   foobar?: string;
+
+  /**
+   * blahblah: numero magico
+   */
   blahblah: number;
 }
 
