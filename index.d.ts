499371d582a2de686a627c1e2eaf45bca53a0338
Andres Arias <aariasc@gmail.com>
2020-09-09 17:08:13 -0600

    Separar en archivos

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,4 @@
-export interface User {
-  /**
-   * firstName: nombre de la persona
-   */
-  firstName: string;
+import { User, Patito } from "./user";
+import { Libro } from "./libro";
 
-  /**
-   * lastName: apellido de la persona
-   */
-  lastName: string;
-
-  /**
-   * age: edad de la persona. Numerico sin decimales
-   */
-  age: number;
-
-  /**
-   * email: correo electronico
-   */
-  email: string;
-
-  /**
-   * favoriteColor: nombre del color favorito
-   */
-  favoriteColor: string;
-
-  /**
-   * foobar: campo magico (opcional)
-   */
-  foobar?: string;
-
-  /**
-   * blahblah: numero magico
-   */
-  blahblah: number;
-}
-
-export interface Libro {
-  author: string;
-  title: string;
-  pub: number;
-}
+export { User, Patito, Libro };
---
+++ libro.ts
@@ -0,0 +1,5 @@
+export interface Libro {
+  author: string;
+  title: string;
+  pub: number;
+}
